50056409550df18ae44bc64029dc4f15a249f231
Ajaycse25 <142646585+Ajaycse25@users.noreply.github.com>
2025-08-21 18:52:55 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -12,7 +12,7 @@ export default function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/register', {
+      const res = await axios.post('https://mini-linkedin-backend-tzir.onrender.com/api/auth/register', {
         name,
         email,
         password,
